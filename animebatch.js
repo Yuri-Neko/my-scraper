cba2ae7587c37769c58af18172273a027f1e046a
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2023-05-07 09:02:01 +0700

    Update animebatch.js

--- animebatch.js
+++ animebatch.js
@@ -1,9 +1,39 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-async function scrapeAnimeBatch() {
+const BaseURL = 'https://www.animebatch.id';
+const Batch = async () => {
   try {
-    const response = await axios.get('https://www.animebatch.id/');
+    const response = await axios.get(BaseURL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const posts = [];
+
+    $('.box-blog').each(function(i, elem) {
+      const postTitle = $(this).find('h2 > a').text().trim();
+      const postLink = $(this).find('h2 > a').attr('href');
+      const postImage = $(this).find('img').attr('src');
+      const postDescription = $(this).find('.exp > p').text().trim();
+      const postDate = $(this).find('.auth > i').text().trim();
+
+      const post = {
+        title: postTitle,
+        link: postLink,
+        image: postImage,
+        description: postDescription,
+        date: postDate
+      };
+
+      posts.push(post);
+    });
+
+    return posts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+const Ongoing = async () => {
+  try {
+    const response = await axios.get(BaseURL);
     const html = response.data;
     const $ = cheerio.load(html);
     const articles = [];
@@ -32,5 +62,35 @@ async function scrapeAnimeBatch() {
     console.log(error);
   }
 }
+const Movie = async () => {
+  try {
+    const response = await axios.get(BaseURL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const movies = [];
+    $('.widget_senction:contains("Movies") .box-blog').each((i, el) => {
+      const title = $(el).find('.data h2 a').text().trim();
+      const url = $(el).find('.data h2 a').attr('href');
+      const image = $(el).find('.img img').attr('src');
+      const desc = $(el).find('.exp p').text().trim();
+      const date = $(el).find('.auth i').text().trim();
 
-module.exports = scrapeAnimeBatch;
+      const movie = {
+        title,
+        url,
+        image,
+        desc,
+        date
+      };
+
+      movies.push(movie);
+    });
+
+    return movies;
+  });
+} catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { Batch, Ongoing, Movie };
