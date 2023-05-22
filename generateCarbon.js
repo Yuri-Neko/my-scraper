f2861484109259663c0471d41f667bba1a707238
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2023-05-22 14:06:32 +0700

    Update generateCarbon.js

--- generateCarbon.js
+++ generateCarbon.js
@@ -13,3 +13,13 @@ async function generateCarbon(options) {
 }
 
 //DARI FATUR https://github.com/Ftwrr/
+
+//example bot ESM
+
+/*let handler = async (m, { text, usedPrefix, command }) => {
+    const code = m.hasQuotedMsg ? m._data.quotedMsg.body : text
+    if (!code) return m.reply(`Input Code:\n${usedPrefix + command} console.log('hello world')`);
+    let carbon = await generateCarbon(code)
+    if (carbon.status !== 200) return m.reply(`${carbon.status} ${carbon.statusText}`);
+    m.reply(new MessageMedia((await fileTypeFromBuffer(carbon.result)).mime, carbon.result.toString("base64")))
+}*/
